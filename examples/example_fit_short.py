d2fc08400dd3c66e0e8ed243b999ceebfc41abd8
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2024-12-18 08:56:55 +0100

    use for loop

--- examples/example_fit_short.py
+++ examples/example_fit_short.py
@@ -3,44 +3,19 @@ import numpy as np
 from n2_fit import N2_fit
 
 
-fwhm_l=113
 nc = N2_fit()
-
-file = 'belchem_n2_short'
-spectra = np.loadtxt(os.path.join('data', file+'.txt'))
-energy = spectra[:, 0]
-intensity = spectra[:, 1]
-nc.fit_n2_3peaks(energy, intensity, 
-                 title=file, dict_fit=None, 
-                 fwhm_l=fwhm_l, 
-                 plot_initial_guess=False, 
-                 print_fit_results=False, 
-                 save_results='results', 
-                 show_results=False)
-
-
-file = 'N1spistarBL25SU_short'
-spectra = np.loadtxt(os.path.join('data', file+'.txt'))
-energy = spectra[:, 0]
-intensity = spectra[:, 1]
-nc.fit_n2_3peaks(energy, intensity, 
-                 title=file, dict_fit=None, 
-                 fwhm_l=fwhm_l, 
-                 plot_initial_guess=False, 
-                 print_fit_results=False, 
-                 save_results='results', 
-                 show_results=False)
+files_list = ['belchem_n2_short', 'N1spistarBL25SU_short', 'belchem_n2_136_short']
+for file in files_list:
+    spectra = np.loadtxt(os.path.join('data', file+'.txt'))
+    energy = spectra[:, 0]
+    intensity = spectra[:, 1]
+    nc.fit_n2_3peaks(energy, intensity, 
+                    title=file, dict_fit=None, 
+                    fwhm_l=114, 
+                    plot_initial_guess=False, 
+                    print_fit_results=False, 
+                    save_results='results', 
+                    show_results=False)
 
 
-file = 'belchem_n2_136_short'
-spectra = np.loadtxt(os.path.join('data', file+'.txt'))
-energy = spectra[:, 0]
-intensity = spectra[:, 1]
-nc.fit_n2_3peaks(energy, intensity, 
-                 title=file, dict_fit=None, 
-                 fwhm_l=fwhm_l, 
-                 plot_initial_guess=False, 
-                 print_fit_results=False, 
-                 save_results='results', 
-                 show_results=False)
 
